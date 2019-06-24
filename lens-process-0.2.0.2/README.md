d5beca027027acf6497159a1f4c5d0a1c50813e8
Emily Pillmore <emily@kadena.io>
2019-06-24 01:11:26 -0400

    drop lower bounds

---
+++ lens-process-0.2.0.2/README.md
@@ -0,0 +1,13 @@
+
+# lens-process
+
+[![Hackage](https://img.shields.io/hackage/v/lens-process.svg)](https://hackage.haskell.org/package/lens-process)
+[![Build Status](https://travis-ci.org/emilypi/lens-process.svg?branch=master)](https://travis-ci.org/emilypi/lens-process)
+
+This package is intended to be on the lighter side, with few dependencies aside from `lens`. For full disclosure, this is the minimal dependency graph of `lens-process`:
+
+![lens-process dependencies](dependencies.jpg)
+
+### Motivation
+
+`lens-process` provides optics for the [process](https://hackage.haskell.org/package/process) package. These optics provide convenient lenses, traversals, and prisms, as well as classy variants for significant classifiable portions of the library for convenience. In addition, we provide some combinators for working with `CreateProcess` types. The intention of this package is to create a well-typed optical layer for `process`, reflecting the shape of certain types of commands at the type level.
---
+++ lens-process-0.2.0.2/src/System/Process/Lens.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE Rank2Types #-}
+-- |
+-- Copyright 	: 2019 Emily Pillmore
+-- License	: BSD
+--
+-- Maintainer	: Emily Pillmore <emilypi@cohomolo.gy>
+-- Stability	: Experimental
+-- Portability	: TypeFamilies, RankNTypes
+--
+-- This module provides all of the optical exports, as well ask any associated
+-- combinators. For just the optics, see 'System.Process.Lens.Optics', or
+-- if you are in need of something lighter weight, just for working with a
+-- 'CreateProcess' in terms of getters and setters, see <https://hackage.haskell.org/package/microlens>
+--
+-- For more information on usage and how to work with lenses, see <http://github.com/ekmett/lens/wiki> and the tests for example uses. You can also ask questions on Freenode
+-- in the #haskell-lens channel.
+--
+module System.Process.Lens
+( -- * Data
+  ProcessHandler(..)
+, -- * Prisms
+  _ShellCommand
+, _RawCommand
+, arguments
+, _Inherit
+, _UseHandle
+, _CreatePipe
+, _NoStream
+  -- * Isos
+, _Handler
+  -- * Lenses
+, cmdspec_
+, cwd_
+, env_
+, stdin_
+, stdout_
+, stderr_
+, closefds
+, creategroup
+, delegatectlc
+#if MIN_VERSION_process(1, 3, 0)
+, detachconsole
+, createnewconsole
+, newsession
+#endif
+#if MIN_VERSION_process(1, 4, 0)
+, childgroup
+, childuser
+#endif
+#if MIN_VERSION_process(1, 5, 0)
+, useprocessjobs
+#endif
+, hstdin
+, hstdout
+, hstderr
+, hhandle
+  -- * Classes
+, IsRaw(..)
+, IsShell(..)
+, IsInherit(..)
+, IsUseHandle(..)
+, IsCreatePipe(..)
+, IsNoStream(..)
+, HasStdin(..)
+, HasStdout(..)
+, HasStderr(..)
+  -- * Combinators
+, inheriting
+, piping
+, handling
+, nostreaming
+, arguing
+, rawOf
+, shellOf
+, usehandleOf
+) where
+
+
+import System.Process.Lens.CommandSpec
+import System.Process.Lens.CreateProcess
+import System.Process.Lens.ProcessHandler
+import System.Process.Lens.StdStream
